5e330ac7e69e7a270cabdaaa5e45c09629e0df5f
SEEE <upamakapramodh@gmail.com>
2026-03-09 23:45:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -142,3 +142,22 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+const slides = document.querySelectorAll(".bg-slide");
+
+let currentSlide = 0;
+
+function changeSlide(){
+
+slides[currentSlide].classList.remove("active");
+
+currentSlide++;
+
+if(currentSlide >= slides.length){
+currentSlide = 0;
+}
+
+slides[currentSlide].classList.add("active");
+
+}
+
+setInterval(changeSlide,5000);
