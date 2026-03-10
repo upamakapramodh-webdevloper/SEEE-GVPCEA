6b2bc81251c11bb4ba6db8aefa54654caae371f5
SEEE <upamakapramodh@gmail.com>
2026-03-10 21:30:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -174,4 +174,18 @@ toggle.innerHTML="🌙";
 }
 
 });
+const themeToggle=document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("click",()=>{
+
+document.body.classList.toggle("dark-mode");
+
+if(document.body.classList.contains("dark-mode")){
+themeToggle.innerHTML="☀️";
+}else{
+themeToggle.innerHTML="🌙";
+}
+
+});
+
 
