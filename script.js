ebba392402b6a61bcfc9a12a980c99e8342b124f
SEEE <upamakapramodh@gmail.com>
2026-03-10 08:16:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -161,3 +161,17 @@ slides[currentSlide].classList.add("active");
 }
 
 setInterval(changeSlide,5000);
+const toggle = document.getElementById("theme-toggle");
+
+toggle.addEventListener("click",function(){
+
+document.body.classList.toggle("dark-mode");
+
+if(document.body.classList.contains("dark-mode")){
+toggle.innerHTML="☀️";
+}else{
+toggle.innerHTML="🌙";
+}
+
+});
+
